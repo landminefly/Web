8ae2b78a43e65c2d601eed73b557ceda6369d0cd
LandmineFly <2353789859@qq.com>
2023-06-04 14:44:23 +0800

    update

--- harbour-logistics-data-platform-test/src/components/module1.vue
+++ harbour-logistics-data-platform-test/src/components/module1.vue
@@ -1,6 +1,17 @@
+<script>
+import fileUpload from './file-upload.vue';
+export default {
+    components:{
+        fileUpload,
+    }
+}
+</script>
+
 <template>
     <div id="module1-wrapper">
-        <div id="upload-file-wrapper"></div>
+        <div id="file-upload-wrapper">
+            <fileUpload></fileUpload>
+        </div>
     </div>
 </template>
 
